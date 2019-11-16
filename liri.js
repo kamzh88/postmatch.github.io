3ab0f88de1170d606341cdfa5707ba94681e6b06
KinKam Zhao <Kamzh88@KinKams-Air.fios-router.home>
2019-11-16 14:54:42 -0500

    log entry into log.txt

--- liri.js
+++ liri.js
@@ -17,48 +17,70 @@ var bitApiKey = (keys.bitApi)
 var action = process.argv[2];
 var value = nodeArgs.slice(3).join(" ");
 
-function run() {
+function run(action, value) {
+    
     var concert = "concert-this";
     var SPOTIFY_THIS_SONG = "spotify-this-song";
     var movie = "movie-this";
     var COMMAND = "do-what-it-says";
     switch (action) {
         case movie:
-            movieAPI();
+            movieAPI(value);
+            log();
             break;
 
         case concert:
-            concertAPI();
+            concertAPI(value);
+            log();
             break;
 
         case SPOTIFY_THIS_SONG:
-            spotifyAPI();
+            spotifyAPI(value);
+            log();
             break;
 
         case COMMAND:
-            command();
+            command(value);
             break;
 
         default:
             console.log("invalid input");
     }
 }
-function movieAPI() {
-    var movieURL = `http://www.omdbapi.com/?y=&plot=short&t=${value}&apikey=${omdbApiKey}`;
-    axios.get(movieURL).then(
-        function (response) {
-            console.log(`Title of Movie: ${response.data.Title}`);
-            console.log(`Release Year: ${response.data.Year}`);
-            console.log(`IMDB movie's rating is: ${response.data.imdbRating}`);
-            console.log(`Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}`);
-            console.log(`Production country: ${response.data.Country}`);
-            console.log(`Language of the movie: ${response.data.Language}`);
-            console.log(`Plot: ${response.data.Plot}`);
-            console.log(`Actors/Actress: ${response.data.Actors}`);
 
-        }
-    )
+function movieAPI() {
+    if (!value) {
+        var movieURL = `http://www.omdbapi.com/?y=&plot=short&t=Mr.+Nobody&apikey=${omdbApiKey}`;
+        axios.get(movieURL).then(
+            function (response) {
+                console.log(`Title of Movie: ${response.data.Title}`);
+                console.log(`Release Year: ${response.data.Year}`);
+                console.log(`IMDB movie's rating is: ${response.data.imdbRating}`);
+                console.log(`Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}`);
+                console.log(`Production country: ${response.data.Country}`);
+                console.log(`Language of the movie: ${response.data.Language}`);
+                console.log(`Plot: ${response.data.Plot}`);
+                console.log(`Actors/Actress: ${response.data.Actors}`);
+            }
+        )
+    } else {
+        var movieURL = `http://www.omdbapi.com/?y=&plot=short&t=${value}&apikey=${omdbApiKey}`;
+        axios.get(movieURL).then(
+            function (response) {
+                console.log(`Title of Movie: ${response.data.Title}`);
+                console.log(`Release Year: ${response.data.Year}`);
+                console.log(`IMDB movie's rating is: ${response.data.imdbRating}`);
+                console.log(`Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}`);
+                console.log(`Production country: ${response.data.Country}`);
+                console.log(`Language of the movie: ${response.data.Language}`);
+                console.log(`Plot: ${response.data.Plot}`);
+                console.log(`Actors/Actress: ${response.data.Actors}`);
+
+            }
+        )
+    };
 }
+
 function concertAPI() {
     var concertURL = `https://rest.bandsintown.com/artists/${value}/events?app_id=${bitApiKey}`;
     console.log(concertURL);
@@ -77,40 +99,63 @@ function concertAPI() {
     )
 }
 
-function spotifyAPI() {
-    spotify.search({ type: 'track', query: value, limit: 1 }, function (err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        }
-        console.log(`Name of Artists: ${data.tracks.items[0].album.artists[0].name}`);
-        console.log(`Name of Song: ${data.tracks.items[0].name}`);
-        console.log(`Album name: ${data.tracks.items[0].album.name}`);
-        console.log(`Preview song: ${data.tracks.items[0].preview_url}`);
-
-    });
+function spotifyAPI(value) {
+    
+    if (!value) {
+        var value = "The Sign";
+        
+        spotify.search({ name: "Ace of Base", type: 'track', query: value}, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            console.log(`Name of Artists: ${data.tracks.items[5].album.artists[0].name}`);
+            console.log(`Name of Song: ${data.tracks.items[5].name}`);
+            console.log(`Album name: ${data.tracks.items[5].album.name}`);
+            console.log(`Preview song: ${data.tracks.items[5].preview_url}`);
+            console.log("----------------------------------------------------")
+        });
+        
+    } else {
+        spotify.search({ type: 'track', query: value}, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            console.log(`Name of Artists: ${data.tracks.items[0].album.artists[0].name}`);
+            console.log(`Name of Song: ${data.tracks.items[0].name}`);
+            console.log(`Album name: ${data.tracks.items[0].album.name}`);
+            console.log(`Preview song: ${data.tracks.items[0].preview_url}`);
+        });
+    }
 }
 
 function command() {
     fs.readFile("random.txt", "utf8", function (error, data) {
+
         if (error) {
             return console.log(error);
         }
-        console.log(data);
 
         var dataArr = data.split(",");
-
-        console.log(dataArr[0]);
 
         action = dataArr[0];
 
         value = dataArr[1];
 
-        run(action);
+        run(action, value);
 
     })
 }
-run();
 
+function log() {
+    fs.appendFile("log.txt", `${value},\n`, function (err) {
+        if (err) {
+            console.log(err);
+        }
+        
+        else {
+            console.log("Content Added!")
+        }
+    })
+}
 
-// if i do movie-this in spiderman it gives me an error.
-// Also command function is still not working.
+run(action, value);
